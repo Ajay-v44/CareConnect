c4898e1d40611d7144a147dd790825d77dc6add6
Ajay v <vajay4834@gmail.com>
2023-07-19 23:22:30 +0530

    Added doctors and departments

--- django1/home/views.py
+++ django1/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Departments,Doctors
 
 # Create your views here.
 def index(request):
@@ -13,8 +14,15 @@ def about(request):
 def booking(request):
     return render(request,'booking.html')
 def doctors(request):
-    return render(request,'doctors.html')
+    dict_docs={
+        'doctors':Doctors.objects.all()
+
+    }
+    return render(request,'doctors.html',dict_docs)
 def contact(request):
     return render(request,'contact.html')
 def department(request):
-    return render(request,'department.html')
+    dict_dept={
+        'dept':Departments.objects.all()
+    }
+    return render(request,'department.html',dict_dept)
